dbe5aca6575bd958002dd6366b8ef37ba93ca117
Umakantk8090 <uk8418805147@gmail.com>
2024-02-17 03:03:15 +0530

    hii

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count,setCount]=useState(0)
+  
+  const addValue=()=>{
+    setCount(count+1)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+       <button onClick={addValue} >count</button> 
     </div>
   )
 }
